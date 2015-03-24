919258f4a062700062f3e9c6c09e727181f0b7ef
Lyusya <ladan_lyusya@mail.ru>
2015-03-24 17:55:47 +0700

    null

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,9 +1,9 @@
 
 public class Hello_World {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
 
-	}
+    }
 
 }
